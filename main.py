14ef5a37d6030b396e53380fb7d77efd3b02fd37
Ricardo Lima <sricardolimaa@gmail.com>
2022-09-28 22:19:07 -0300

    poo python

--- main.py
+++ main.py
@@ -8,5 +8,15 @@ class Pessoa:
     def dias_vividos(self):
         return (date.today() - self.data_nascimento).days
 
+    @property
+    def data_nascimento(self):
+        return self._data_nascimento
+
+    @data_nascimento.setter
+    def data_nascimento(self, value):
+        if not isinstance(value, date):
+            raise ValueError('data_nascimento é um objeto tipo date')
+        self._data_nascimento = value
+
 p1 = Pessoa('Ricardo', date(1999, 6, 14))
 print("A quantidade de dias vividos foram: ", p1.dias_vividos())
